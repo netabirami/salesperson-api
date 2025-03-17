063139470fd7c96a4d17e10d01c52aa3a38eb286
netabirami <net.abirami@gmail.com>
2025-03-17 19:52:06 +0100

    docker changes

--- src/test/java/com/salfri/salesperson_api/SalespersonApiApplicationTest.java
+++ src/test/java/com/salfri/salesperson_api/SalespersonApiApplicationTest.java
@@ -33,7 +33,7 @@ public class SalespersonApiApplicationTest {
     @Test
     public void shouldCreateAndTestSalesPerson() throws Exception {
 
-        RestAssured.baseURI = "http://localhost:8080/sales-api";
+        RestAssured.baseURI = "http://localhost:8080/sales";
 
         // Step 1: Create a new Salesperson via POST
         String requestBody = "{\"name\":\"Abi\", " +
